c88326d6e50e05d19fad90d839b39e8abcc20c6e
unknown <abilewomo@gmail.com>
2023-03-29 15:45:29 -0400

    add readme file

--- script.js
+++ script.js
@@ -1,35 +1,88 @@
+//Define Game class
 class Game {
-
-    startGame(){
-        document.body.insertBefore(this.createStage(), document.body.childNodes[0]);
-        //document.body.append(this.createStage())
-    }
-    createStage(){
-       const stage = document.createElement("canvas")
-       stage.width = "600"
-       stage.height="400"
-       stage.context = stage.getContext("2d")
-       return stage
+  constructor(canvas) {
+    this.ctx = canvas.getContext("2d");
+  }
+  startGame() {}
+  reset() {
+    const ctx = this.ctx;
+    ctx.font = "30px Arial";
+    ctx.fillText("Start", 180, 180);
+  }
+  // clear(canvas){
+  //     const ctx = this.ctx
+  //     ctx.clearRect(0, 0, canvas.width, canvas.height)
+  // }
+  update() {
+    const ctx = this.ctx;
   }
 }
-  
 
+//Define Player class
 class Player {
+  constructor(canvas) {
+    this.canvas = canvas;
+    this.ctx = canvas.getContext("2d");
+    this.xposition = 0;
+    this.yposition = 560;
+    this.draw();
+	window.addEventListener('keydown', (event) => this.move(event));
+  }
 
-}
+  draw() {
+    const ctx = this.ctx;
+    ctx.fillStyle = "blue";
 
-class fallingObjects {
-    constructor(name){
-        this.name = name
+    ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    ctx.fillRect(this.xposition , this.yposition, 80, 40);
+  }
+  move(event) {
+    if (event.key === "ArrowLeft" && this.xposition > 0) {
+      // Left arrow
+      this.xposition -= 10;
+    }else if (event.key === "ArrowRight" && this.xposition < 700) {
+      // Right arrow
+      this.xposition += 10;
+    }else if (event.key === "ArrowUp") { // Up arrow
+        this.yposition -= 10;
+    } else if (event.key === "ArrowDown") { // Down arrow
+        this.yposition += 10;
     }
 
+    this.draw();
+  }
 }
 
-class objectFactory{
-    objects = []
+//Define Objects class
+class Objects {
+  constructor(canvas) {
+    this.ctx = canvas.getContext("2d");
+  }
+  draw() {
+    const ctx = this.ctx;
+    ctx.beginPath();
+    ctx.arc(25, 25, 20, 0, 2 * Math.PI);
+    ctx.fillStyle = "red";
+    ctx.fill();
+  }
 }
-let newGame = new Game()
-newGame.startGame()
+
+class objectFactory {
+  objects = [];
+}
+//Variable declaration
+const canvas = document.querySelector("#canvas");
+
+let newGame = new Game(canvas);
+let newPlayer = new Player(canvas);
+let newObject = new Objects(canvas);
+newPlayer.draw();
+//newObject.draw();
+
+//document.addEventListener("load", newGame.reset());
+//window.addEventListener("keydown", newPlayer.move);
+
+//
 // STEPS
 // 1. create objects that fall randomly
 // 2. create a container to catch the falling objects
@@ -38,6 +91,4 @@ newGame.startGame()
 // 5. some levels should have faster moving objects
 // 6. Game is won when all levels are passed
 // 7. add a counter
-// 8. add sound 
-
-
+// 8. add sound
